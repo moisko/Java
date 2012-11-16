d2f7e685b6208f22851139701c881ad0894df4be
moisko <moisko@gmail.com>
2012-11-16 23:37:36 +0200

    add new implementation

--- Multithreading/src/com/github/multithreading/Main.java
+++ Multithreading/src/com/github/multithreading/Main.java
@@ -1,8 +1,46 @@
 package com.github.multithreading;
 
+import com.github.multithreading.api.IStore;
+import com.github.multithreading.impl.Store;
+
 public class Main {
 
-	public static void main(String[] args) {
+	private static final int STORE_CAPACITY = 100;
 
+	public static void main(String[] args) {
+		IStore store = new Store(STORE_CAPACITY);
+
+		createAndStartFirstPCPair(store);
+
+		createAndStartSecondPCPair(store);
+
+		createAndStartThirdPCPair(store);
+	}
+
+	private static void createAndStartFirstPCPair(IStore store) {
+		Producer p1 = new Producer(store);
+		Consumer c1 = new Consumer(store);
+		Thread pt1 = new Thread(p1);
+		Thread ct1 = new Thread(c1);
+		pt1.start();
+		ct1.start();
+	}
+
+	private static void createAndStartSecondPCPair(IStore store) {
+		Producer p2 = new Producer(store);
+		Consumer c2 = new Consumer(store);
+		Thread pt2 = new Thread(p2);
+		Thread ct2 = new Thread(c2);
+		pt2.start();
+		ct2.start();
+	}
+
+	private static void createAndStartThirdPCPair(IStore store) {
+		Producer p3 = new Producer(store);
+		Consumer c3 = new Consumer(store);
+		Thread pt3 = new Thread(p3);
+		Thread ct3 = new Thread(c3);
+		pt3.start();
+		ct3.start();
 	}
 }
