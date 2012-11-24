81f792e7871915deccf93e8875b5760cfef178f3
Boris Borisov <boris.borisov@sap.com>
2012-11-24 14:58:02 +0200

    close with Closeable

--- Networking/src/com/github/networking/utils/Safe.java
+++ Networking/src/com/github/networking/utils/Safe.java
@@ -1,14 +1,13 @@
 package com.github.networking.utils;
 
-import java.io.BufferedReader;
+import java.io.Closeable;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Safe {
 
-	public static void close(ServerSocket connection) {
+	public static void close(Closeable connection) {
 		if (connection != null) {
 			try {
 				connection.close();
@@ -28,19 +27,14 @@ public class Safe {
 		}
 	}
 
-	public static void close(BufferedReader br) {
-		if (br != null) {
+	public static void close(ServerSocket connection) {
+		if (connection != null) {
 			try {
-				br.close();
+				connection.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
 
-	public static void close(PrintWriter writer) {
-		if (writer != null) {
-			writer.close();
-		}
-	}
 }
