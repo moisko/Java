072b8764ce0697e1c33a1e5e1623c8725b3eee59
moisko <moisko@gmail.com>
2013-10-12 12:43:52 +0300

    swap m and n if m<n

--- Algorithms/src/com/github/algorithms/GreatestCommonDivisor.java
+++ Algorithms/src/com/github/algorithms/GreatestCommonDivisor.java
@@ -3,17 +3,21 @@ package com.github.algorithms;
 public class GreatestCommonDivisor {
 
 	public static int gcd(int m, int n) {
-		int r = m % n;
-		if (r == 0)
-			return n;
-		// Set m <- n
-		// Set n <- r
-		return gcd(n, r);
+		if (m >= n) {
+			int r = m % n;
+			if (r == 0)
+				return n;
+			// Set m <- n
+			// Set n <- r
+			return gcd(n, r);
+		}
+		return gcd(n, m);
 	}
 
 	public static void main(String[] args) {
 		System.out.println("gcd(5, 3)=" + gcd(5, 3));
 		System.out.println("gcd(6, 5)=" + gcd(6, 5));
-		System.out.println("gcd(15, 5)=" + gcd(15, 5));
+		System.out.println("gcd(119, 544)=" + gcd(119, 544));
+		System.out.println("gcd(544, 119)=" + gcd(544, 119));
 	}
 }
