1a4d58eabae8b1c9c3e06b5020a9fc939a3da2ea
moisko <moisko@gmail.com>
2013-10-12 12:54:59 +0300

    check functions args

--- Algorithms/src/com/github/algorithms/GreatestCommonDivisor.java
+++ Algorithms/src/com/github/algorithms/GreatestCommonDivisor.java
@@ -3,6 +3,7 @@ package com.github.algorithms;
 public class GreatestCommonDivisor {
 
 	public static int gcd(int m, int n) {
+		checkArgs(m, n);
 		if (m >= n) {
 			int r = m % n;
 			if (r == 0)
@@ -11,8 +12,20 @@ public class GreatestCommonDivisor {
 			// Set n <- r
 			return gcd(n, r);
 		}
-		// m <-> n
+		// Exchange m <-> n
 		return gcd(n, m);
+	}
+
+	private static void checkArgs(int m, int n) {
+		String message = null;
+		if (m < 0) {
+			message = m + " must be positive";
+		} else if (n < 0) {
+			message = n + " must be positive";
+		}
+		if (message != null) {
+			throw new IllegalArgumentException(message);
+		}
 	}
 
 	public static void main(String[] args) {
@@ -20,5 +33,6 @@ public class GreatestCommonDivisor {
 		System.out.println("gcd(6, 5)=" + gcd(6, 5));
 		System.out.println("gcd(119, 544)=" + gcd(119, 544));
 		System.out.println("gcd(544, 119)=" + gcd(544, 119));
+		System.out.println("gcd(-544, 119)=" + gcd(-544, 119));
 	}
 }
